0017a31e3ccea73667b1a15347a704e82623b548
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-05-26 13:30:25 -0700

    copy + pasting from docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io
+use tui::Terminal
+use tui::backend::CrosstermBackend
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, Result,
+    event,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend);
 }
