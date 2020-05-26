cd18c2a2bf90ec6cc6dde5a8329b4a129ccf31c2
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-05-26 16:15:38 -0700

    testing stuff i guess

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::io
-use tui::Terminal
-use tui::backend::CrosstermBackend
+use std::io::{stdout, Write};
+use tui::{backend::CrosstermBackend, Terminal};
 use crossterm::{
     execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
@@ -9,7 +8,14 @@ use crossterm::{
 };
 
 fn main() {
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend);
+//    let stdout = io::stdout();
+//    let backend = CrosstermBackend::new(stdout);
+//    let mut terminal = Terminal::new(backend);
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Styled text here."),
+        ResetColor
+    );
 }
