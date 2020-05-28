aa87aa45c2b4c83f06774ddb64b687c507f45223
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-05-28 11:40:54 -0700

    error handling????

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
 #![allow(array_into_iter)]
-use std::{env, fs, io};
-use tui::Terminal;
-use tui::backend::TermionBackend;
-use termion::raw::IntoRawMode;
-use termion::{terminal_size, clear::All, cursor::Goto};
+use std::{env, fs, io, process};
+use tui::{backend::TermionBackend, Terminal};
+use termion::{raw::IntoRawMode, terminal_size, clear::All, cursor::Goto};
 use tui::widgets::{Widget, Table, Borders, Block, Row};
 use tui::layout::{Layout, Constraint, Direction, Rect};
 use tui::style::{Style, Color};
 
+fn check_args(args: Vec<String>) -> Result<String, &'static str> {
+    if args.len() < 2 {
+        return Err("please specify a splits file");
+    }
+    let splits = &args[1];
+    Ok(splits.to_string())
+}
+
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
-    //println!("{:?}", args[1]);
     print!("{}{}", All, Goto(1, 1));
+    let file = check_args(args).unwrap_or_else(|err| {
+        println!("{}", err);
+        process::exit(1);
+    });
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    //let splits = fs::read_to_string(&args[1]);
     terminal.draw(|mut f| {
-        let area = Rect::new(0, 0, 75, 25);
+        let area = Rect::new(0, 0, 50, 25);
         let test = Table::new(
             ["Split", "Delta", "Time"].iter(),
             vec![
@@ -27,11 +35,11 @@ fn main() -> Result<(), io::Error> {
                 Row::Data(["Split 4", "Diff4", "Time4"].iter())
             ].into_iter()
         )
-        .block(Block::default().title("Table"))
+        .block(Block::default().title("RSplit").borders(Borders::ALL))
         .header_style(Style::default().fg(Color::Yellow))
         .widths(&[Constraint::Length(5), Constraint::Length(5), Constraint::Length(10)])
         .style(Style::default().fg(Color::White))
-        .column_spacing(10);
+        .column_spacing(5);
         f.render_widget(test, area)
     })
 }
