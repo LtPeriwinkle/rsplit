50a77bf07cf4b8d084ea4ffb6c119887b6b6f09f
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-05-28 10:17:06 -0700

    clears garbage before printing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 #![allow(array_into_iter)]
-use std::io;
+use std::{env, fs, io};
 use tui::Terminal;
 use tui::backend::TermionBackend;
 use termion::raw::IntoRawMode;
-use termion::terminal_size;
+use termion::{terminal_size, clear::All, cursor::Goto};
 use tui::widgets::{Widget, Table, Borders, Block, Row};
 use tui::layout::{Layout, Constraint, Direction, Rect};
-use tui::style::Style;
-use tui::style::Color;
+use tui::style::{Style, Color};
 
 fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+    //println!("{:?}", args[1]);
+    print!("{}{}", All, Goto(1, 1));
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    let row_style = Style::default().fg(Color::White);
+    //let splits = fs::read_to_string(&args[1]);
     terminal.draw(|mut f| {
-        //let area: Rect = (0, 0, rect.Result.0, rect.Result.1);
-        let area = Rect::new(12, 12, 75, 25);
+        let area = Rect::new(0, 0, 75, 25);
         let test = Table::new(
-            ["Split", "Delta", "Time"].into_iter(),
+            ["Split", "Delta", "Time"].iter(),
             vec![
-                Row::Data(["Split 1", "Row12", "Row13"].into_iter()),
-                Row::Data(["Split 2", "Row22", "Row23"].into_iter()),
-                Row::Data(["Split 3", "Row32", "Row33"].into_iter()),
-                Row::Data(["Split 4", "Row42", "Row43"].into_iter())
+                Row::Data(["Split 1", "Diff1", "Time1"].iter()),
+                Row::Data(["Split 2", "Diff2", "Time2"].iter()),
+                Row::Data(["Split 3", "Diff3", "Time3"].iter()),
+                Row::Data(["Split 4", "Diff4", "Time4"].iter())
             ].into_iter()
         )
         .block(Block::default().title("Table"))
