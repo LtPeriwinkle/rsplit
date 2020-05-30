f5b48c7bd3cf4ed8a321ee1b9ebccec6f64738be
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-05-30 14:48:17 -0700

    all functions can now use same terminal

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![allow(array_into_iter)]
 use std::{env, fs, io, process};
-use tui::{backend::TermionBackend, Terminal};
-use termion::{raw::IntoRawMode, clear::All, cursor::Goto};
+use tui::backend::TermionBackend;
+use tui::terminal::Terminal;
+use termion::{clear::All, cursor::Goto};
+use termion::raw::IntoRawMode;
 use tui::widgets::{Table, Borders, Block, Row, List, Text};
-use tui::layout::{Rect};
-use tui::style::{Style, Color};
+use tui::layout::Rect;
+use tui::style::{Style, Color, Modifier};
+
+static standard: tui::style::Color = Color::White;
 
 fn check_args(args: Vec<String>) -> Result<String, &'static str> {
     if args.len() < 2 {
@@ -14,17 +18,28 @@ fn check_args(args: Vec<String>) -> Result<String, &'static str> {
     Ok(splits.to_string())
 }
 
+fn draw_split_names(mut terminal: tui::terminal::Terminal<tui::backend::TermionBackend<termion::raw::RawTerminal<std::io::Stdout>>>) -> Result<(), io::Error> {
+    terminal.draw(|mut s| {
+        let area = Rect::new(0, 0, 20, 18);
+        let splits = ["asdf", "qwerty", "ghjkl"].iter().map(|i| Text::raw(*i));
+        let names = List::new(splits)
+            .block(Block::default().title("Split Name").title_style(Style::default().modifier(Modifier::BOLD | Modifier::UNDERLINED))
+                .borders(Borders::RIGHT | Borders::TOP))
+            .style(Style::default().fg(standard));
+        s.render_widget(names, area)
+    })
+}
+
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
     let file = check_args(args).unwrap_or_else(|err| {
         eprintln!("{}", err);
         process::exit(1);
     });
+    print!("{}{}", All, Goto(1, 1));
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    let standard = Color::White;
-    print!("{}{}", All, Goto(1, 1));
     /*terminal.draw(|mut f| {
         let area = Rect::new(0, 0, 50, 25);
         let test = Table::new(
@@ -43,12 +58,5 @@ fn main() -> Result<(), io::Error> {
         .column_spacing(5);
         f.render_widget(test, area)
     })*/
-    terminal.draw(|mut s| {
-        let area = Rect::new(0, 0, 20, 18);
-        let splits = ["asdf", "qwerty", "ghjkl"].iter().map(|i| Text::raw(*i));
-        let names = List::new(splits)
-            .block(Block::default().title("Split Name").borders(Borders::RIGHT | Borders::TOP))
-            .style(Style::default().fg(standard));
-        s.render_widget(names, area)
-    })
+    draw_split_names(terminal)
 }
