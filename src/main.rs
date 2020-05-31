725f365b21fcaa22a15edb08afaca165cd0910e6
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-05-30 20:32:21 -0700

    set up table to be the way i want

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-#![allow(array_into_iter)]
 use std::{env, fs, io, process};
-use tui::backend::TermionBackend;
-use tui::terminal::Terminal;
+use tui::{backend::TermionBackend, terminal::Terminal};
 use termion::{clear::All, cursor::Goto};
 use termion::raw::{IntoRawMode, RawTerminal};
 use tui::widgets::{Table, Borders, Block, Row, List, Text};
-use tui::layout::Rect;
+use tui::layout::{Rect, Constraint};
 use tui::style::{Style, Color, Modifier};
 
+static good: tui::style::Color = Color::Green;
 static standard: tui::style::Color = Color::White;
 
 fn check_args(args: Vec<String>) -> Result<String, &'static str> {
@@ -19,23 +18,23 @@ fn check_args(args: Vec<String>) -> Result<String, &'static str> {
 }
 
 fn draw_timer(mut terminal: Terminal<TermionBackend<RawTerminal<io::Stdout>>>) -> Result<(), io::Error> {
-    terminal.draw(|mut f| {
-        let area = Rect::new(0, 0, 50, 25);
-        let test = Table::new(
-            ["Split", "Delta", "Time"].iter(),
+    terminal.draw(|mut t| {
+        let area = Rect::new(0, 0, 35, 18);
+        let timer = Table::new(
+            ["Split", "Time"].iter(),
             vec![
-                Row::Data(["Split 1", "Diff1", "Time1"].iter()),
-                Row::Data(["Split 2", "Diff2", "Time2"].iter()),
-                Row::Data(["Split 3", "Diff3", "Time3"].iter()),
-                Row::Data(["Split 4", "Diff4", "Time4"].iter())
+                Row::StyledData(["12345678901234567890", "Time1"].iter(), Style::default().fg(good)),
+                Row::Data(["Split 2", "Time2"].iter()),
+                Row::Data(["Split 3", "Time3"].iter()),
+                Row::Data(["Split 4", "12345678912"].iter())
             ].into_iter()
         )
-        .block(Block::default().title("RSplit").borders(Borders::ALL))
-        .header_style(Style::default().fg(Color::Yellow))
-        .widths(&[Constraint::Length(5), Constraint::Length(5), Constraint::Length(10)])
-        .style(Style::default().fg(Color::White))
-        .column_spacing(5);
-        f.render_widget(test, area)
+        .block(Block::default().title("RSplit").borders(Borders::LEFT | Borders::TOP).title_style(Style::default().modifier(Modifier::BOLD)))
+        .header_style(Style::default().fg(Color::White).modifier(Modifier::UNDERLINED))
+        .widths(&[Constraint::Length(20), Constraint::Length(11)])
+        .style(Style::default().fg(standard))
+        .column_spacing(2);
+        t.render_widget(timer, area)
     })
 }
 
@@ -45,31 +44,13 @@ fn main() -> Result<(), io::Error> {
         eprintln!("{}", err);
         process::exit(1);
     });
-    /*let splits_json = fs::read_to_string(file).unwrap_or_else(|err| {
+    let splits_json = fs::read_to_string(file).unwrap_or_else(|err| {
         eprintln!("{}", err);
         process::exit(1);
-    });*/
+    });
     print!("{}{}", All, Goto(1, 1));
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    /*terminal.draw(|mut f| {
-        let area = Rect::new(0, 0, 50, 25);
-        let test = Table::new(
-            ["Split", "Delta", "Time"].iter(),
-            vec![
-                Row::Data(["Split 1", "Diff1", "Time1"].iter()),
-                Row::Data(["Split 2", "Diff2", "Time2"].iter()),
-                Row::Data(["Split 3", "Diff3", "Time3"].iter()),
-                Row::Data(["Split 4", "Diff4", "Time4"].iter())
-            ].into_iter()
-        )
-        .block(Block::default().title("RSplit").borders(Borders::ALL))
-        .header_style(Style::default().fg(Color::Yellow))
-        .widths(&[Constraint::Length(5), Constraint::Length(5), Constraint::Length(10)])
-        .style(Style::default().fg(Color::White))
-        .column_spacing(5);
-        f.render_widget(test, area)
-    })*/
     draw_timer(terminal)
 }
