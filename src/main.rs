1900c04bda65cb4272053cf77eaed2d6793d61eb
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-06-07 12:33:11 -0700

    commented out everything lol

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::{env, fs, io, process, iter::FromIterator, thread::sleep, time::Duration};
-use tui::{backend::TermionBackend, terminal::Terminal};
-use termion::{clear::All, cursor::Goto};
-use termion::raw::{IntoRawMode, RawTerminal};
-use tui::widgets::{Table, Borders, Block, Row, Paragraph, Text};
-use tui::layout::{Rect, Constraint, Alignment};
-use tui::style::{Style, Color, Modifier};
+use std::{env, fs, process, iter::FromIterator, thread::sleep, time::Duration};
+use std::io::{Write, stdout, Error};
+use crossterm::Result as cross_result;
+use crossterm::{QueueableCommand};
+use crossterm::style::Print;
 use serde::{Serialize, Deserialize};
 use serde_json;
 
@@ -14,54 +12,43 @@ struct Split {
     time: u32,
 }
 
-//i really really did not want to end up typing this multiple times
+/*i really really did not want to end up typing this multiple times
 type Output<'a> = &'a mut Terminal<TermionBackend<RawTerminal<io::Stdout>>>;
 
 //these are the colors that the timer will use for ahead/behind/gold/other stuff
 static GOOD: Color = Color::LightGreen;
 static STANDARD: Color = Color::White;
 static BAD: Color = Color::LightRed;
-static GOLD: Color = Color::LightYellow;
+static GOLD: Color = Color::LightYellow;*/
 
 //makes sure that an argument was actually provided
 fn check_args(args: Vec<String>) -> Result<String, &'static str> {
     if args.len() < 2 {
-        return Err("please specify a file to read splits from");
+        Err("please specify a file to read splits from")
     }
     let splits = &args[1];
     Ok(splits.to_string())
 }
 
-//draws the timer window to the terminal, including area with splits, title, and soon-to-be current time
-fn draw_timer(terminal: Output, rows: Vec<tui::widgets::Row<core::slice::Iter<String>>>) -> Result<(), io::Error> {
-    terminal.draw(|mut t| {
-        let area = Rect::new(0, 0, 35, 18);
-        let time_table = Table::new(
-            ["Split", "Time"].iter(),
-            rows.into_iter()
-        )
-        .block(Block::default().title("RSplit").borders(Borders::LEFT | Borders::TOP).title_style(Style::default().modifier(Modifier::BOLD)))
-        .header_style(Style::default().fg(STANDARD).modifier(Modifier::UNDERLINED))
-        .widths(&[Constraint::Length(20), Constraint::Length(11)])
-        .style(Style::default().fg(STANDARD))
-        .column_spacing(2);
-        t.render_widget(time_table, area);
-        let time_area = Rect::new(0, 19, 35, 2);
-        let text = [Text::raw("words")];
-        let time = Paragraph::new(text.iter())
-            .alignment(Alignment::Right);
-        t.render_widget(time, time_area)
-    })
+fn print_to_terminal() -> cross_result<()> {
+    let mut stdout = stdout();
+    stdout.queue(Print("something is being printed"))?;
+    stdout.flush()?;
+    Ok(())
 }
+//draws the timer window to the terminal, including area with splits, title, and soon-to-be current time
+/*fn draw_timer(terminal: Output, rows: Vec<tui::widgets::Row<core::slice::Iter<String>>>) -> Result<(), io::Error> {
 
-fn splits_to_print<'a>(split_vec: &'a Vec<Row<core::slice::Iter<'a, String>>>, line: usize) -> Vec<Row<std::slice::Iter<'a, String>>> {
+}*/
+
+/*fn splits_to_print<'a>(split_vec: &'a Vec<Row<core::slice::Iter<'a, String>>>, line: usize) -> Vec<Row<std::slice::Iter<'a, String>>> {
     let end = line + 1;
     //i have no idea why line works but it does. thank you rust forum user nemo157.
     let print_vec = Vec::from_iter(split_vec[line..end].iter().cloned());
     print_vec
-}
+}*/
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     let second = Duration::new(1, 0);
     let mut current_line = 0;
@@ -74,9 +61,9 @@ fn main() -> Result<(), io::Error> {
         process::exit(1);
     });
     let json_as_splits: Vec<Split> = serde_json::from_str(&json_raw)?;
-    print!("{}{}", All, Goto(1, 1));
-    let stdout = io::stdout().into_raw_mode()?;
-    let backend = TermionBackend::new(stdout);
+    print_to_terminal();
+    //print!("{}{}", All, Goto(1, 1));
+    /*let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     let num_of_splits = json_as_splits.len();
 
@@ -91,6 +78,6 @@ fn main() -> Result<(), io::Error> {
             break;
         }
     }
-    print!("{}", Goto(1, 21));
+    print!("{}", Goto(1, 21));*/
     Ok(())
 }
