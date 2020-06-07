c7338b8494c794404f9c6abef601781e997785a2
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-06-07 15:01:12 -0700

    slightly less of a mess

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{env, fs, process, iter::FromIterator, thread::sleep, time::Duration};
 use std::io::{Write, stdout, Error};
 use crossterm::Result as cross_result;
-use crossterm::{QueueableCommand};
+use crossterm::{QueueableCommand, ExecutableCommand};
 use crossterm::style::Print;
 use serde::{Serialize, Deserialize};
 use serde_json;
@@ -24,32 +24,28 @@ static GOLD: Color = Color::LightYellow;*/
 //makes sure that an argument was actually provided
 fn check_args(args: Vec<String>) -> Result<String, &'static str> {
     if args.len() < 2 {
-        Err("please specify a file to read splits from")
+        return Err("please specify a file to read splits from");
     }
     let splits = &args[1];
     Ok(splits.to_string())
 }
 
-fn print_to_terminal() -> cross_result<()> {
-    let mut stdout = stdout();
+fn print_to_terminal(mut stdout: std::io::Stdout, rows: Vec<&str>) -> cross_result<()> {
     stdout.queue(Print("something is being printed"))?;
     stdout.flush()?;
     Ok(())
 }
-//draws the timer window to the terminal, including area with splits, title, and soon-to-be current time
-/*fn draw_timer(terminal: Output, rows: Vec<tui::widgets::Row<core::slice::Iter<String>>>) -> Result<(), io::Error> {
 
-}*/
-
-/*fn splits_to_print<'a>(split_vec: &'a Vec<Row<core::slice::Iter<'a, String>>>, line: usize) -> Vec<Row<std::slice::Iter<'a, String>>> {
+fn splits_to_print(split_vec: Vec<&str>, line: usize) -> Vec<&str> {
     let end = line + 1;
     //i have no idea why line works but it does. thank you rust forum user nemo157.
     let print_vec = Vec::from_iter(split_vec[line..end].iter().cloned());
     print_vec
-}*/
+}
 
 fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
+    let out = stdout();
     let second = Duration::new(1, 0);
     let mut current_line = 0;
     let file = check_args(args).unwrap_or_else(|err| {
@@ -61,12 +57,15 @@ fn main() -> Result<(), Error> {
         process::exit(1);
     });
     let json_as_splits: Vec<Split> = serde_json::from_str(&json_raw)?;
-    print_to_terminal();
-    //print!("{}{}", All, Goto(1, 1));
-    /*let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    let num_of_splits = json_as_splits.len();
-
+    print_to_terminal(out).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+    let mut rows = Vec::new();
+    for i in json_as_splits {
+        let split = i.name;
+        split_vec.push(something);
+    }
     loop {
         {
             let table_rows = splits_to_print(&rows, current_line);
@@ -78,6 +77,6 @@ fn main() -> Result<(), Error> {
             break;
         }
     }
-    print!("{}", Goto(1, 21));*/
+    //print!("{}", Goto(1, 21));
     Ok(())
 }
