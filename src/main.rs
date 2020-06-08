755cffbf4fcbb2df6e8045e42284f423ae0bcea4
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-06-07 21:04:39 -0700

    trivial stuff now rebuilt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::{env, fs, process, iter::FromIterator, thread::sleep, time::Duration};
 use std::io::{Write, stdout, Error};
 use crossterm::Result as cross_result;
-use crossterm::{QueueableCommand, ExecutableCommand};
-use crossterm::style::Print;
+use crossterm::{QueueableCommand, execute};
+use crossterm::style::{Print, Color, SetForegroundColor};
+use crossterm::terminal::{Clear, ClearType::All};
 use serde::{Serialize, Deserialize};
 use serde_json;
 
@@ -12,14 +13,12 @@ struct Split {
     time: u32,
 }
 
-/*i really really did not want to end up typing this multiple times
-type Output<'a> = &'a mut Terminal<TermionBackend<RawTerminal<io::Stdout>>>;
-
 //these are the colors that the timer will use for ahead/behind/gold/other stuff
-static GOOD: Color = Color::LightGreen;
+static GOOD: Color = Color::Green;
 static STANDARD: Color = Color::White;
-static BAD: Color = Color::LightRed;
-static GOLD: Color = Color::LightYellow;*/
+static BAD: Color = Color::Red;
+static GOLD: Color = Color::Yellow;
+static RESET: Color = Color::Reset;
 
 //makes sure that an argument was actually provided
 fn check_args(args: Vec<String>) -> Result<String, &'static str> {
@@ -30,13 +29,15 @@ fn check_args(args: Vec<String>) -> Result<String, &'static str> {
     Ok(splits.to_string())
 }
 
-fn print_to_terminal(mut stdout: std::io::Stdout, rows: Vec<&str>) -> cross_result<()> {
-    stdout.queue(Print("something is being printed"))?;
+fn print_to_terminal(mut stdout: std::io::Stdout) -> cross_result<()> {
+    stdout.queue(Clear(All))?
+        .queue(SetForegroundColor(GOOD))?
+        .queue(Print("something is being printed"))?;
     stdout.flush()?;
     Ok(())
 }
 
-fn splits_to_print(split_vec: Vec<&str>, line: usize) -> Vec<&str> {
+fn splits_to_print<'a>(split_vec: &'a Vec<String>, line: usize) -> Vec<String> {
     let end = line + 1;
     //i have no idea why line works but it does. thank you rust forum user nemo157.
     let print_vec = Vec::from_iter(split_vec[line..end].iter().cloned());
@@ -54,29 +55,30 @@ fn main() -> Result<(), Error> {
     });
     let json_raw = fs::read_to_string(file).unwrap_or_else(|err| {
         eprintln!("{}", err);
-        process::exit(1);
+        process::exit(2);
     });
     let json_as_splits: Vec<Split> = serde_json::from_str(&json_raw)?;
     print_to_terminal(out).unwrap_or_else(|err| {
         eprintln!("{}", err);
-        process::exit(1);
+        process::exit(3);
     });
     let mut rows = Vec::new();
     for i in json_as_splits {
         let split = i.name;
-        split_vec.push(something);
+        rows.push(split);
     }
     loop {
         {
             let table_rows = splits_to_print(&rows, current_line);
             sleep(second);
             current_line += 1;
-            draw_timer(&mut terminal, table_rows)?;
+            //draw_timer(&mut terminal, table_rows)?;
         }
         if current_line == 2 {
             break;
         }
     }
     //print!("{}", Goto(1, 21));
+    execute!(stdout(), SetForegroundColor(RESET));
     Ok(())
 }
