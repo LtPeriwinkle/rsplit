a19acbbbae27a73ceb9a4fd8b71de6be7b60a01b
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-06-20 19:20:34 -0700

    move check_args to components.rs

--- src/components.rs
+++ src/components.rs
@@ -13,3 +13,13 @@ pub static GOOD: Color = Color::Green;
 pub static BAD: Color = Color::Red;
 pub static GOLD: Color = Color::Yellow;*/
 pub static RESET: Color = Color::Reset;
+
+//makes sure that an argument was actually provided and returns an error that is used to stop program later if no argument
+pub fn check_args(args: Vec<String>) -> Result<String, &'static str> {
+    if args.len() < 2 {
+        return Err("please specify a file to read splits from");
+    }
+    //since for some reason args().collect() gives an array with the command also in it, grab filename out of the array
+    let splits = &args[1];
+    Ok(splits.to_string())
+}
--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,6 @@ use serde_json;
 mod components;
 use components::*;
 
-//makes sure that an argument was actually provided and returns an error that is used to stop program later if no argument
-fn check_args(args: Vec<String>) -> Result<String, &'static str> {
-    if args.len() < 2 {
-        return Err("please specify a file to read splits from");
-    }
-    //since for some reason args().collect() gives an array with the command also in it, grab filename out of the array
-    let splits = &args[1];
-    Ok(splits.to_string())
-}
-
 //make the json file into a vec of Splits (from components.rs) and the split names into another vec
 fn get_splits<'a>(file: &'a String, _vec: Vec<Split>) -> (Vec<Split<'a>>, Vec<&'a str>) {
     let mut rows = Vec::new();
@@ -37,7 +27,7 @@ fn get_splits<'a>(file: &'a String, _vec: Vec<Split>) -> (Vec<Split<'a>>, Vec<&'
     (json_as_splits, rows)
 }
 
-//rewrote this function to get rid of out of bounds accessing
+//gets a chunk of the full vec of split names to print each time
 fn splits_to_print<'a>(split_vec: &'a Vec<&str>, line: usize) -> Vec<&'a str> {
     if split_vec.len() < 18 {
         //i want a better way to do this but im lazy and this was fast to write
@@ -52,7 +42,7 @@ fn splits_to_print<'a>(split_vec: &'a Vec<&str>, line: usize) -> Vec<&'a str> {
 }
 
 //prints everything that needs to be shown, by queueing timer rows then flushing them at the end
-fn print_timer(out: &mut std::io::Stdout, rows: &Vec<&str>) -> cross_result<()> {
+fn print_timer(out: &mut std::io::Stdout, rows: Vec<&str>) -> cross_result<()> {
     let mut current_line = 0;
     loop {
             //introduce a new scope to print new rows each iteration
@@ -66,13 +56,13 @@ fn print_timer(out: &mut std::io::Stdout, rows: &Vec<&str>) -> cross_result<()> 
 
             }
         }
-        //makes crossterm do all the stuff we queued in queue_table_row() calls
+        //makes crossterm do all the stuff queued in queue_table_row() calls
         out.flush()?;
     Ok(())
 }
 
 //takes the name of the split, the time that should be displayed for it, and the row in terminal where it should be printed
-//and queues a row with name + time into the buffer
+//and queues a row with name + time into the crossterm buffer
 fn queue_table_row(split_name: &str, time: &str, out: &mut std::io::Stdout, row: u16) -> cross_result<()> {
     out.queue(MoveTo(1, row))?
         .queue(SetForegroundColor(GOOD))?
@@ -91,18 +81,22 @@ fn main() -> Result<(), Error> {
     let mut out = stdout();
 
     //deal with the json stuff, yeah its ugly but it gets the job done
-    let json_raw = fs::read_to_string(file)?;
+    let json_raw = fs::read_to_string(file).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(2);
+    });
     let useless_item: Vec<Split> = Vec::new();
     let results = get_splits(&json_raw, useless_item);
     let names = results.1;
+    //will be used for time comparisons later, not useful right now
     let _split_vec = results.0;
 
     //make sure we arent printing over other stuff
     out.execute(Clear(All)).unwrap();
 
-    //gave the loop a name because it will eventually have another loop inside
+    //gave the loop a name because it will eventually have another loop inside and actually need to be a loop
     'main: loop {
-        print_timer(&mut out, &names).unwrap_or_else(|err| {eprintln!("{}", err); process::exit(3)});
+        print_timer(&mut out, names).unwrap_or_else(|err| {eprintln!("{}", err); process::exit(3)});
         break 'main;
     }
 
